bdc70e276a5792559d144a0a485c97f769bb4fc9
Ray Lee <rhlee@berkeley.edu>
2017-02-03 16:27:27 -0800

    DRYD-69: Move Popup into cspace-layout.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Panel } from './components/Panel';
+export { default as Popup } from './components/Popup';
 export { default as Row } from './components/Row';
 export { default as Table } from './components/Table';
