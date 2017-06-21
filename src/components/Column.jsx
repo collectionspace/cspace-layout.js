31bd30910739bf60196d6304ede49b2875eb5b33
Yousuf Nejati <yousufnejati@gmail.com>
2017-06-21 11:38:42 -0700

    Fix ESLINT errors.

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from '../../styles/cspace-layout/Column.css';
 
-const propTypes = {
+export default function Column(props) {
+  const {
+    right,
+    left,
+    children,
+  } = props;
+
+  const classes = classNames(
+    styles.normal, right ? styles.right : '', left ? styles.left : ''
+  );
+
+  return (
+    <div className={classes}>
+      {children}
+    </div>
+  );
+}
+
+Column.propTypes = {
   children: PropTypes.node,
   right: PropTypes.bool,
   left: PropTypes.bool,
 };
-
-export default class Column extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-
-    const {
-      right,
-      left,
-      children,
-    } = this.props;
-
-    const classes = classNames(
-      styles.normal, right ? styles.right : '', left ? styles.left : ''
-    );
-
-    return (
-      <div className={classes}>
-        {children}
-      </div>
-    );
-  };
-}
-
-Column.propTypes = propTypes;
-
