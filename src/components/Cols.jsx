5b01bc8177b0f64dd5bdac5d049b15f00ee38595
Yousuf Nejati <yousufnejati@gmail.com>
2017-06-23 12:30:49 -0700

    Fix Cols an alias of Row and not div.

--- src/components/Cols.jsx
+++ src/components/Cols.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Row from './Row';
 
+// An alias of the Row component
 export default function Cols(props) {
   const {
     children,
   } = props;
 
   return (
-    <div>
+    <Row>
       {children}
-    </div>
+    </Row>
   );
 }
 
